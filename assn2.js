e06f3ff6f9b7d994855ee8a95980a9850a8d76a7
bigyanlamsal <58266429+bigyanlamsal@users.noreply.github.com>
2022-04-30 20:43:55 +0545

    Add files via upload

---
+++ assn2.js
@@ -0,0 +1,42 @@
+const formulatePayment = (choice, amount) => {
+    
+    //initial payment
+    const initialPayment = ((10 / 100) * amount).toFixed(2);
+    const remainingTotal = amount - initialPayment;
+
+    //if players choice is weekly
+    if (choice == 'weekly') {
+    let weeklyAmountPayment = 0;
+    let weekly = remainingTotal / weeklyAmountPayment;
+    while (weekly > 156) {
+    weeklyAmountPayment = weeklyAmountPayment + 50;
+    let weeks = remainingTotal / weeklyAmountPayment;
+    if (weeks % 50 !== 0) {
+    weekly = Math.ceil(weeks);
+    }
+    }
+    console.log(`You will be paid ${weeklyAmountPayment} for
+    ${weekly} weeks.`);
+    return { time: weekly, amount: weeklyAmountPayment };
+    } 
+
+
+    //if players choice is monthly
+    else if (choice == 'monthly') {
+    let monthlyPrice = 0;
+    
+    let monthly = remainingTotal / monthlyPrice;
+    while (monthly > 36) {
+    monthlyPrice = monthlyPrice + 100;
+    let months = remainingTotal / monthlyPrice;
+    if (months % 100 !== 0) {
+    monthly = Math.ceil(months);
+    }
+    }
+    console.log(`You will be paid ${monthlyPrice} for $
+    {monthly} months.`);
+    return { time: monthly, amount: monthlyPrice };
+    } else {
+    return null;
+    }
+    };
